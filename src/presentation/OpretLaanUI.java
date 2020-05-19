42856d4a0e82835ac4dc0d7f853a29bf4af8cb9a
Kerbz-dev <mglarsen22@gmail.com>
2020-05-19 11:16:00 +0200

    ui update

--- src/presentation/OpretLaanUI.java
+++ src/presentation/OpretLaanUI.java
@@ -15,10 +15,408 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import logic.GetKV;
+import logic.GetKV.kreditRating;
 import logic.laanCheckDuplicate;
 import logic.opretLaan;
 
 public class OpretLaanUI {
+
+	    DecimalFormat numberFormatter = new DecimalFormat("0");
+	    private TextField navnTField;
+//	    private TextField adrTField;
+	    private TextField tlfTField;
+	    private TextField PostnrTField;
+	    private TextField ByTField;
+	    private TextField VejTField;
+	    private TextField HusnrTField;
+	    private TextField cprTField;
+	    private TextField mailTField;
+	    private TextField bilnavnTField;
+	    private TextField bilprisTField;
+	    private TextField udbetalingTField;
+	    private TextField laengdeTField;
+	    private Button opretLaanBtn;
+	    private Stage opretLaanStage;
+	    private laanCheckDuplicate laanlogic = new laanCheckDuplicate();
+	    private String kundenavn;
+	    private String adr;
+	    private String tlf;
+	    private String cpr;
+	    private String mail;
+	    private String bilnavn;
+	    private String bilpris;
+	    private String udbetalingspris;
+	    private String laanleangde;
+	    private Label opretStatusLbl;
+	    private Scene scene3;
+	    private Pane pane3;
+	    private Image ferrari;
+	    private ImageView ferraripic;
+
+
+	    public void start() {
+	        opretLaanStage = new Stage();
+	        opretLaanStage.setTitle("Ferrari lånesystem");
+	        opretLaanStage.getIcons()
+	                .add(new Image("https://i.pinimg.com/564x/c9/87/c8/c987c8a5c896fca22c5cfbd62edb7359.jpg"));
+
+
+	        Label lgnNameLbl = new Label("logget in som " + "username");
+
+
+	        pane3 = new Pane();
+	        ferrari = new Image(
+	                "https://3.bp.blogspot.com/-DRM75enaO7s/VDrpAiCm55I/AAAAAAAABGM/VnsBvuXIygU/s1600/Ferrari%2BCar%2Blogos.jpg%22");
+	        pane3.setPrefHeight(670.0);
+	        ferraripic = new ImageView();
+
+
+	        // Button assignments
+	        opretLaanBtn = new Button("Opret lånetilbud");
+
+
+	        navnTField = new TextField();
+	    //adrTField = new TextField();
+	        PostnrTField = new TextField();
+	        ByTField = new TextField();
+	        VejTField = new TextField();
+	        HusnrTField = new TextField();
+	        tlfTField = new TextField();
+	        cprTField = new TextField();
+	        mailTField = new TextField();
+	        bilnavnTField = new TextField();
+	        bilprisTField = new TextField();
+	        udbetalingTField = new TextField();
+	        laengdeTField = new TextField();
+	        opretStatusLbl = new Label("test");
+	        navnTField.relocate(298.0, 230.0);
+	        navnTField.setPrefHeight(25.0);
+	        navnTField.setPrefWidth(321.0);
+	        PostnrTField.relocate(298.0, 255.0);
+	        PostnrTField.setPrefHeight(25.0);
+	        PostnrTField.setPrefWidth(321.0);
+	        ByTField.setPrefWidth(321.0);
+	        ByTField.relocate(298.0, 285.0); 
+	        ByTField.setPrefHeight(25.0);
+	        VejTField.setPrefWidth(321.0);
+	        VejTField.relocate(298.0, 315.0); 
+	        VejTField.setPrefHeight(25.0);
+	        HusnrTField.setPrefWidth(321.0);
+	        HusnrTField.relocate(298.0, 345.0); 
+	        HusnrTField.setPrefHeight(25.0);
+	        tlfTField.relocate(298.0, 375.0); 
+	        tlfTField.setPrefHeight(25.0);
+	        tlfTField.setPrefWidth(321.0);
+	        cprTField.relocate(298, 405.0); 
+	        cprTField.setPrefHeight(25.0);
+	        cprTField.setPrefWidth(321.0);
+	        mailTField.relocate(298, 435); 
+	        mailTField.setPrefHeight(25.0);
+	        mailTField.setPrefWidth(321.0);
+	        bilnavnTField.relocate(298, 465);
+	        bilnavnTField.setPrefHeight(25.0);
+	        bilnavnTField.setPrefWidth(321.0);
+	        bilprisTField.relocate(298, 495);
+	        bilprisTField.setPrefHeight(25.0);
+	        bilprisTField.setPrefWidth(321.0);
+	        udbetalingTField.relocate(298, 525);
+	        udbetalingTField.setPrefHeight(25.0);
+	        udbetalingTField.setPrefWidth(321.0);
+	        laengdeTField.relocate(298, 555);
+	        laengdeTField.setPrefHeight(25.0);
+	        laengdeTField.setPrefWidth(321.0);
+	        opretStatusLbl.setFont(new Font(24));
+	        opretLaanBtn.relocate(350, 600);
+
+
+	        ferraripic.setFitWidth(350);
+	        ferraripic.setFitHeight(175);
+	        ferraripic.setImage(ferrari);
+	        ferraripic.relocate(282, 30);
+	        pane3.setPrefHeight(556);
+	        pane3.setPrefWidth(842);
+
+
+	        pane3.setPrefWidth(930.0);
+
+
+	        // adrTField.relocate(arg0, arg1);
+	        laengdeTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        udbetalingTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        bilprisTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        bilnavnTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        mailTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        cprTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        tlfTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        PostnrTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        navnTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        ByTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        VejTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        HusnrTField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	        pane3.setStyle("-fx-background-color: #ff2800");
+
+
+	        laengdeTField.setPromptText("Lånets længde:");
+	        udbetalingTField.setPromptText("Udbetaling:");
+	        bilprisTField.setPromptText("Bilpris:");
+	        bilnavnTField.setPromptText("Bilnavn:");
+	        mailTField.setPromptText("E-Mail:");
+	        cprTField.setPromptText("CPR-nr:");
+	        tlfTField.setPromptText("Tlf.nr:");
+	        PostnrTField.setPromptText("Postnr:");
+	        HusnrTField.setPromptText("Husnr:");
+	        VejTField.setPromptText("Vejnavn:");
+	        ByTField.setPromptText("By:");
+	        navnTField.setPromptText("Navn:");
+
+	    
+	    lgnNameLbl.setPrefHeight(17);
+	        lgnNameLbl.setPrefWidth(200);
+	        lgnNameLbl.relocate(750, 625);
+
+
+	        // Make TFields number only accept number inputs
+	        cprTField.setTextFormatter(new TextFormatter<>(c -> {
+	            if (c.getControlNewText().isEmpty()) {
+	                return c;
+	            }
+
+
+	            ParsePosition parsePosition = new ParsePosition(0);
+	            Object object = numberFormatter.parse(c.getControlNewText(), parsePosition);
+
+
+	            if (object == null || parsePosition.getIndex() < c.getControlNewText().length()) {
+	                return null;
+	            } else {
+	                return c;
+	            }
+	        }));
+
+
+	        tlfTField.setTextFormatter(new TextFormatter<>(c -> {
+	            if (c.getControlNewText().isEmpty()) {
+	                return c;
+	            }
+
+
+	            ParsePosition parsePosition = new ParsePosition(0);
+	            Object object = numberFormatter.parse(c.getControlNewText(), parsePosition);
+
+
+	            if (object == null || parsePosition.getIndex() < c.getControlNewText().length()) {
+	                return null;
+	            } else {
+	                return c;
+	            }
+	        }));
+
+
+	        bilprisTField.setTextFormatter(new TextFormatter<>(c -> {
+	            if (c.getControlNewText().isEmpty()) {
+	                return c;
+	            }
+
+
+	            ParsePosition parsePosition = new ParsePosition(0);
+	            Object object = numberFormatter.parse(c.getControlNewText(), parsePosition);
+
+
+	            if (object == null || parsePosition.getIndex() < c.getControlNewText().length()) {
+	                return null;
+	            } else {
+	                return c;
+	            }
+	        }));
+
+
+	        udbetalingTField.setTextFormatter(new TextFormatter<>(c -> {
+	            if (c.getControlNewText().isEmpty()) {
+	                return c;
+	            }
+
+
+	            ParsePosition parsePosition = new ParsePosition(0);
+	            Object object = numberFormatter.parse(c.getControlNewText(), parsePosition);
+
+
+	            if (object == null || parsePosition.getIndex() < c.getControlNewText().length()) {
+	                return null;
+	            } else {
+	                return c;
+	            }
+	        }));
+
+
+	        laengdeTField.setTextFormatter(new TextFormatter<>(c -> {
+	            if (c.getControlNewText().isEmpty()) {
+	                return c;
+	            }
+
+
+	            ParsePosition parsePosition = new ParsePosition(0);
+	            Object object = numberFormatter.parse(c.getControlNewText(), parsePosition);
+
+
+	            if (object == null || parsePosition.getIndex() < c.getControlNewText().length()) {
+	                return null;
+	            } else {
+	                return c;
+	            }
+	        }));
+
+
+	        // Font sizes
+	        opretLaanBtn.setFont(new Font(24));
+	        lgnNameLbl.setFont(new Font(12));
+	        laengdeTField.setFont(new Font(14));
+	        udbetalingTField.setFont(new Font(14));
+	        bilprisTField.setFont(new Font(14));
+	        bilnavnTField.setFont(new Font(14));
+	        mailTField.setFont(new Font(14));
+	        cprTField.setFont(new Font(14));
+	        tlfTField.setFont(new Font(14));
+	        PostnrTField.setFont(new Font(14));
+	        HusnrTField.setFont(new Font(14));
+	        VejTField.setFont(new Font(14));
+	        ByTField.setFont(new Font(14));
+	        
+	        // Add to pane
+	        pane3.getChildren().addAll(navnTField, PostnrTField, tlfTField, cprTField, mailTField, bilnavnTField,
+	                HusnrTField    ,VejTField,ByTField,bilprisTField, udbetalingTField, laengdeTField, opretLaanBtn, opretStatusLbl, ferraripic, lgnNameLbl);
+
+
+	        // Starting opretLaan event when opretLaanBtn is clicked
+	        opretLaanBtn.setOnAction(e -> opretLaan());
+
+
+	        // Show scene
+	        scene3 = new Scene(pane3, 950, 670);
+	        opretLaanStage.setScene(scene3);
+	        opretLaanStage.show();
+	    }
+
+
+	    private void opretLaan() {
+	        kundenavn = navnTField.getText();
+	        adr = PostnrTField.getText();
+	        tlf = tlfTField.getText();
+	        cpr = cprTField.getText();
+	        mail = mailTField.getText();
+	        bilnavn = bilnavnTField.getText();
+	        bilpris = bilprisTField.getText();
+	        udbetalingspris = udbetalingTField.getText();
+	        laanleangde = laengdeTField.getText();
+
+
+	        
+	        if (fieldsEmpty() == true) {
+	            laanFail();
+
+
+	        } else if (cprTField.getText().length() != 10) {
+	            cprFail();
+	        } else if (laanlogic.LaanCheckDuplicate(cpr, tlf) == true) {
+	            laanDuplicate();
+	        }
+
+
+	        else {
+	            checkRating();
+	        }
+	    }
+
+
+	    private boolean fieldsEmpty() {
+	        if (kundenavn.isEmpty() || adr.isEmpty() || tlf.isEmpty() || cpr.isEmpty() || mail.isEmpty()
+	                || bilnavn.isEmpty() || bilpris.isEmpty() || udbetalingspris.isEmpty() || laanleangde.isEmpty()) {
+
+
+	            return true;
+	        }
+	        return false;
+	    }
+
+
+	    private void laanSuccess() {
+	        opretLaan OpretLaan = new opretLaan();
+	        
+	        opretStatusLbl.setText("Lån successfuldt oprettet!");
+	        opretStatusLbl.setTextFill(Color.LIGHTGREEN);
+	        opretStatusLbl.relocate(323, 503);
+	        OpretLaan.CreateLaan(bilnavn, bilpris, udbetalingspris, laanleangde, tlf, kundenavn, cpr, mail, adr);
+	    }
+
+
+	    private void cprFail() {
+	        opretStatusLbl.setText("Der skal være 10 CPR-cifre. Nuværende antal: " + cprTField.getText().length());
+	        opretStatusLbl.setTextFill(Color.WHITE);
+	        opretStatusLbl.relocate(200, 503);
+	    }
+
+
+	    private void laanDuplicate() {
+	        opretStatusLbl.setText("Bruger eksisterer allerede med valgte CPR og/eller tlfnr!");
+	        opretStatusLbl.setTextFill(Color.WHITE);
+	        opretStatusLbl.relocate(152, 503);
+
+
+	    }
+
+
+	    private void laanFail() {
+	        opretStatusLbl.setText("Udfyld venligst alle felter!");
+	        opretStatusLbl.setTextFill(Color.WHITE);
+	        opretStatusLbl.relocate(325, 503);
+
+
+	    }
+
+	    private void kreditRatingFail() {
+	        opretStatusLbl.setText("Kundens kreditværdighed er under den tilladte grænse!");
+	        opretStatusLbl.setTextFill(Color.WHITE);
+	        opretStatusLbl.relocate(155, 503);
+	    }
+	    
+	    private void systemError() {
+	        opretStatusLbl.setText("Der opstod en fejl i systemet! Genstart venligst programmet.");
+	        opretStatusLbl.setTextFill(Color.WHITE);
+	        opretStatusLbl.relocate(150, 503);
+	    }
+
+
+	    private void checkRating() {
+	        GetKV getKV = new GetKV();
+	        
+	        switch (getKV.getKreditvaerdighed(cpr)) {
+	        case rateA:
+	            laanSuccess();
+	            System.out.println("Kunden er oprettet" + "\n" + "rating: A");
+	            break;
+	        case rateB:
+	            laanSuccess();
+	            System.out.println("Kunden er oprettet" + "\n" + "rating: B");
+	            break;
+	        case rateC:
+	            laanSuccess();
+	            System.out.println("Kunden er oprettet" + "\n" + "rating: C");
+	            break;
+	        case rateD:
+	            kreditRatingFail();
+	            System.out.println("Fejl: " + "\n" + "Kunden er rating: D");
+	            
+	            break;
+	        case error:
+	            System.out.println("major ERROR BOY");
+	            systemError();
+	            break;
+	        }
+	    }
+
+
+	}
+
+	/*
 	DecimalFormat numberFormatter = new DecimalFormat("0");
 	private TextField navnTField;
 	private TextField adrTField;
@@ -263,9 +661,10 @@ public class OpretLaanUI {
 			laanDuplicate();
 		}
 
-		else {
+		else if (laanlogic.LaanCheckDuplicate(cpr, tlf) == false ){
 			checkRating();
 		}
+		else systemError();
 	}
 
 	private boolean fieldsEmpty() {
@@ -337,7 +736,6 @@ public class OpretLaanUI {
 		case rateD:
 			kreditRatingFail();
 			System.out.println("Fejl: " + "\n" + "Kunden er rating: D");
-			
 			break;
 		case error:
 			System.out.println("major ERROR BOY");
@@ -346,4 +744,4 @@ public class OpretLaanUI {
 		}
 	}
 
-}
+}*/
